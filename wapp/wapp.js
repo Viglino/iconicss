fd019310eaa1998386272c0e2f34c3820cc1e467
Jean-Marc Viglino <jean-marc.viglino@ign.fr>
2017-11-05 16:34:43 +0100

    [UPD] change icon size to 1em

---
+++ wapp/wapp.js
@@ -0,0 +1,45 @@
+﻿// Debug mode
+var debug = /debug/.test(window.location.search);
+if (debug) $("body").addClass("debug");
+
+// Search function
+$(".search input").on("keyup", function()
+{    var rex = new RegExp($(this).val(),"gi");
+    $(".icons i").each(function(){
+        if (rex.test($(this).attr('class'))) $(this).parent().show();
+        else $(this).parent().hide();
+    });
+});
+
+// Change icon
+function showICSS()
+{	$(".icons > div").removeClass("selected");
+    var i = $("i",this).attr("class");
+    $(".icon i").removeClass().addClass("icss-anim "+i);
+    $(".icon .title").text(i.replace(/icss[\ |-]/g, ""));
+    $(this).addClass("selected");
+};
+
+// Load config
+$.ajax("config.json",{
+    dataType: "json",
+    cache: false,
+    success:function(r) {
+        var n=0;
+        for(i in r.icons)
+        {	console.log(i);
+            n++;
+            $('<link/>', { rel: 'stylesheet', href: "css/"+i+".css" }).appendTo('head');
+            var div = $("<div>").click(showICSS).appendTo(".icons");
+            $("<i>").attr('title',i)
+                .data("icss",i)
+                .addClass('icss-'+i)
+                .appendTo(div);
+        }
+        $(".icons div").first().click();
+        $(".nb").text(n);
+    },
+    error:function(){
+
+    }
+});
