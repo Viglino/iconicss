d91d8564e39199dba5a2df62fe892b0af317d954
Jean-Marc Viglino <jean-marc.viglino@ign.fr>
2017-11-18 19:08:17 +0100

    [ADD) package

---
+++ gulpfile.js
@@ -0,0 +1,27 @@
+﻿/** Gulp file to create dist
+*/
+var gulp = require("gulp");
+var autoprefixer = require('gulp-autoprefixer');
+const concat = require('gulp-concat');
+const cssmin = require('gulp-cssmin');
+
+gulp.task('css', function () {
+    gulp.src(['icss.css','./css/*.css'])
+        .pipe(autoprefixer('last 10 versions', 'ie 10'))
+		.pipe(concat('iconicss.css'))
+		.pipe(gulp.dest('./dist'));
+});
+
+gulp.task('cssmin', function () {
+    gulp.src(['icss.css','./css/*.css'])
+        .pipe(autoprefixer({
+            browsers: ['last 2 versions'],
+            cascade: false
+		}))
+		.pipe(concat('iconicss.min.css'))
+		.pipe(cssmin())
+		.pipe(gulp.dest('./dist'));
+});
+
+// The default task that will be run if no task is supplied
+gulp.task("default", ["css", "cssmin"]);
