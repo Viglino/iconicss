985eb54d2d6931cfd7bc3591d76874b84e1b7233
Jean-Marc Viglino <jean-marc.viglino@ign.fr>
2018-01-13 15:46:45 +0100

    [ADD] custom build

--- gulpfile.js
+++ gulpfile.js
@@ -5,6 +5,7 @@ var autoprefixer = require('gulp-autoprefixer');
 const concat = require('gulp-concat');
 const cssmin = require('gulp-cssmin');
 
+/** Autoprefix CSS + minification */
 gulp.task('css', function () {
     gulp.src(['icss.css','./css/*.css'])
         .pipe(autoprefixer('last 10 versions', 'ie 10'))
@@ -19,6 +20,18 @@ gulp.task('cssmin', function () {
             cascade: false
 		}))
 		.pipe(concat('iconicss.min.css'))
+		.pipe(cssmin())
+		.pipe(gulp.dest('./dist'));
+});
+
+/** Create a custom build using the custom dir */
+gulp.task('custom', function () {
+    gulp.src(['icss.css','./custom/*.css'])
+        .pipe(autoprefixer({
+            browsers: ['last 2 versions'],
+            cascade: false
+		}))
+		.pipe(concat('myiconicss.css'))
 		.pipe(cssmin())
 		.pipe(gulp.dest('./dist'));
 });
