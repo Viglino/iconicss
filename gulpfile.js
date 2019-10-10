0dfc61c9be4855f1e86c1f387caa9277850c3cfb
Wouter Dingemanse <wouter@agf.nl>
2019-10-10 15:09:21 +0200

    Upgrade packages and add gulp bump

--- gulpfile.js
+++ gulpfile.js
@@ -4,37 +4,52 @@ var gulp = require("gulp");
 var autoprefixer = require('gulp-autoprefixer');
 const concat = require('gulp-concat');
 const cssmin = require('gulp-cssmin');
+const bump = require('gulp-bump');
 
 /** Autoprefix CSS + minification */
-gulp.task('css', function () {
-    gulp.src(['icss.css','./css/*.css'])
+function css() {
+    return gulp
+        .src(['icss.css','./css/*.css'])
         .pipe(autoprefixer('last 10 versions', 'ie 10'))
 		.pipe(concat('iconicss.css'))
 		.pipe(gulp.dest('./dist'));
-});
+};
 
-gulp.task('cssmin', function () {
-    gulp.src(['icss.css','./css/*.css'])
+function mincss() {
+    return gulp
+        .src(['icss.css','./css/*.css'])
         .pipe(autoprefixer({
-            browsers: ['last 2 versions'],
             cascade: false
 		}))
 		.pipe(concat('iconicss.min.css'))
 		.pipe(cssmin())
 		.pipe(gulp.dest('./dist'));
-});
+};
 
 /** Create a custom build using the custom dir */
-gulp.task('custom', function () {
-    gulp.src(['icss.css','./custom/*.css'])
+function custom() {
+    return gulp
+        .src(['icss.css','./custom/*.css'])
         .pipe(autoprefixer({
-            browsers: ['last 2 versions'],
             cascade: false
 		}))
 		.pipe(concat('myiconicss.css'))
 		.pipe(cssmin())
 		.pipe(gulp.dest('./dist'));
-});
+};
 
-// The default task that will be run if no task is supplied
-gulp.task("default", ["css", "cssmin"]);
+function dobump() {
+    return gulp
+        .src('./package.json')
+        .pipe(bump({type:'minor'}))
+        .pipe(gulp.dest('./'));
+
+};
+
+const build = gulp.series(gulp.parallel(css, mincss));
+
+exports.css = css;
+exports.mincss = mincss;
+exports.custom = custom;
+exports.dobump = dobump;
+exports.default = build;
