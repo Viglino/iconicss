de16932f0fe2d607bb6682a27f339336b921a881
Jean-Marc Viglino <jean-marc.viglino@ign.fr>
2020-04-13 09:47:40 +0200

    [ADD] new symbol icons

--- gulpfile.js
+++ gulpfile.js
@@ -8,42 +8,41 @@ const bump = require('gulp-bump');
 
 /** Autoprefix CSS + minification */
 function css() {
-    return gulp
-        .src(['icss.css','./css/*.css'])
-        .pipe(autoprefixer('last 10 versions', 'ie 10'))
-		.pipe(concat('iconicss.css'))
-		.pipe(gulp.dest('./dist'));
+  return gulp
+    .src(['icss.css','./css/*.css'])
+    .pipe(autoprefixer('last 10 versions', 'ie 10'))
+  .pipe(concat('iconicss.css'))
+  .pipe(gulp.dest('./dist'));
 };
 
 function mincss() {
-    return gulp
-        .src(['icss.css','./css/*.css'])
-        .pipe(autoprefixer({
-            cascade: false
-		}))
-		.pipe(concat('iconicss.min.css'))
-		.pipe(cssmin())
-		.pipe(gulp.dest('./dist'));
+  return gulp
+    .src(['icss.css','./css/*.css'])
+    .pipe(autoprefixer({
+      cascade: false
+  }))
+  .pipe(concat('iconicss.min.css'))
+  .pipe(cssmin())
+  .pipe(gulp.dest('./dist'));
 };
 
 /** Create a custom build using the custom dir */
 function custom() {
-    return gulp
-        .src(['icss.css','./custom/*.css'])
-        .pipe(autoprefixer({
-            cascade: false
-		}))
-		.pipe(concat('myiconicss.css'))
-		.pipe(cssmin())
-		.pipe(gulp.dest('./dist'));
+return gulp
+    .src(['icss.css','./custom/*.css'])
+    .pipe(autoprefixer({
+      cascade: false
+  }))
+  .pipe(concat('myiconicss.css'))
+  .pipe(cssmin())
+  .pipe(gulp.dest('./dist'));
 };
 
 function dobump() {
-    return gulp
-        .src('./package.json')
-        .pipe(bump({type:'minor'}))
-        .pipe(gulp.dest('./'));
-
+  return gulp
+    .src('./package.json')
+    .pipe(bump({type:'minor'}))
+    .pipe(gulp.dest('./'));
 };
 
 const build = gulp.series(gulp.parallel(css, mincss));
